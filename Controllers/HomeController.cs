a5898ab2faaa84b75d9ed5f1ac78d66eef311565
LucasEspLau <lukaespinoza12@gmail.com>
2024-04-27 10:30:29 -0500

    Update crear cuenta + html y db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using examen_parcial_cuentas.Models;
+using examen_parcial_cuentas.Models.Entity;
+using examen_parcial_cuentas.Data;
 
 namespace examen_parcial_cuentas.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
+
     }
 
     public IActionResult Index()
@@ -25,6 +31,18 @@ public class HomeController : Controller
     public IActionResult Cuenta()
     {
         return View();
+    }
+     [HttpPost]
+
+    public async Task<IActionResult> Create(Cuenta objCuenta)
+    {
+        _context.Add(objCuenta);
+        await _context.SaveChangesAsync();
+        
+        var message = $"Estimado(a) {objCuenta.Titular}, se creo su cuenta";
+        TempData["Message"] = message;
+        return View("~/Views/Home/Cuenta.cshtml");
+
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
